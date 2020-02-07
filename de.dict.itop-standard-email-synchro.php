439d484216428cdc3bbfbc7b6d7038a63dc123cf
Robert Jaehne <robert.jaehne@itomig.de>
2020-02-07 12:37:23 +0100

    :globe_with_meridians: Update DE-dictionary for iTop 2.7.0

--- de.dict.itop-standard-email-synchro.php
+++ de.dict.itop-standard-email-synchro.php
@@ -80,16 +80,16 @@ Dict::Add('DE DE', 'German', 'Deutsch', array(
 //
 
 Dict::Add('DE DE', 'German', 'Deutsch', array(
-	'Class:MailInboxStandard/Attribute:target_class/Value:Change' => 'Change~~',
-	'Class:MailInboxStandard/Attribute:target_class/Value:Change+' => '~~',
-	'Class:MailInboxStandard/Attribute:target_class/Value:RoutineChange' => 'RoutineChange~~',
-	'Class:MailInboxStandard/Attribute:target_class/Value:RoutineChange+' => '~~',
-	'Class:MailInboxStandard/Attribute:target_class/Value:NormalChange' => 'NormalChange~~',
-	'Class:MailInboxStandard/Attribute:target_class/Value:NormalChange+' => '~~',
-	'Class:MailInboxStandard/Attribute:target_class/Value:EmergencyChange' => 'EmergencyChange~~',
-	'Class:MailInboxStandard/Attribute:target_class/Value:EmergencyChange+' => '~~',
-	'Class:MailInboxStandard/Attribute:target_class/Value:Problem' => 'Problem~~',
-	'Class:MailInboxStandard/Attribute:target_class/Value:Problem+' => '~~',
-	'Class:MailInboxStandard/Attribute:debug_trace' => 'Debug trace~~',
-	'Class:MailInboxStandard/Attribute:debug_trace+' => '~~',
+	'Class:MailInboxStandard/Attribute:target_class/Value:Change' => 'Change',
+	'Class:MailInboxStandard/Attribute:target_class/Value:Change+' => '',
+	'Class:MailInboxStandard/Attribute:target_class/Value:RoutineChange' => 'Routine Change',
+	'Class:MailInboxStandard/Attribute:target_class/Value:RoutineChange+' => '',
+	'Class:MailInboxStandard/Attribute:target_class/Value:NormalChange' => 'Normaler Change',
+	'Class:MailInboxStandard/Attribute:target_class/Value:NormalChange+' => '',
+	'Class:MailInboxStandard/Attribute:target_class/Value:EmergencyChange' => 'Emergency Change',
+	'Class:MailInboxStandard/Attribute:target_class/Value:EmergencyChange+' => '',
+	'Class:MailInboxStandard/Attribute:target_class/Value:Problem' => 'Problem',
+	'Class:MailInboxStandard/Attribute:target_class/Value:Problem+' => '',
+	'Class:MailInboxStandard/Attribute:debug_trace' => 'Debug Trace',
+	'Class:MailInboxStandard/Attribute:debug_trace+' => '',
 ));
