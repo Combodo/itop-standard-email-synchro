9b11932780def7d5ef33eadea5c4027629d00113
vdumas <vincent.dumas@combodo.com>
2024-06-05 13:04:22 +0200

    N°7528 - Improve usability with field label and tooltip on mailbox object

--- en.dict.itop-standard-email-synchro.php
+++ en.dict.itop-standard-email-synchro.php
@@ -21,19 +21,30 @@
  * along with iTop. If not, see <http://www.gnu.org/licenses/>
  */
 
+//
+// Class: MailInboxStandard
+//
+
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
 	'Class:MailInboxStandard' => 'Standard Mail Inbox',
 	'Class:MailInboxStandard+' => 'Source of incoming eMails',
+
 	'Class:MailInboxStandard/Attribute:behavior' => 'Behavior',
 	'Class:MailInboxStandard/Attribute:behavior/Value:create_only' => 'Create new Tickets',
 	'Class:MailInboxStandard/Attribute:behavior/Value:update_only' => 'Update existing Tickets',
 	'Class:MailInboxStandard/Attribute:behavior/Value:both' => 'Create or Update Tickets',
+    'Class:MailInboxStandard/Attribute:behavior+' => 'Behavior when a new message arrives in the inbox:
+- Create or Update: Update a matching Ticket found, otherwise create. 
+- Create new ticket: Every new message creates a new Ticket.
+- Update existing ticket: Update a matching Ticket found, otherwise flag in error.',
 
 	'Class:MailInboxStandard/Attribute:email_storage' => 'After processing the eMail',
 	'Class:MailInboxStandard/Attribute:email_storage/Value:keep' => 'Keep it on the mail server',
 	'Class:MailInboxStandard/Attribute:email_storage/Value:delete' => 'Delete it immediately',
-	'Class:MailInboxStandard/Attribute:email_storage/Value:move' => 'Move to another folder',
+	'Class:MailInboxStandard/Attribute:email_storage/Value:move' => 'Move it to another folder',
+    'Class:MailInboxStandard/Attribute:email_storage+' => 'Select the action to be taken after successfully processing an incoming eMail.
+eMails in error are not in scope of this setting, they are handled using the \'Behavior in case of error\' setting.',
 
 	'Class:MailInboxStandard/Attribute:target_folder' => 'Target folder',
 	'Class:MailInboxStandard/Attribute:target_folder+' => 'Only used to move an email with the IMAP protocol',
@@ -41,44 +52,93 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:MailInboxStandard/Attribute:target_class' => 'Ticket Class',
 	'Class:MailInboxStandard/Attribute:target_class/Value:Incident' => 'Incident',
 	'Class:MailInboxStandard/Attribute:target_class/Value:UserRequest' => 'User Request',
+    'Class:MailInboxStandard/Attribute:target_class/Value:Change' => 'Change',
+    'Class:MailInboxStandard/Attribute:target_class/Value:Change+' => '',
+    'Class:MailInboxStandard/Attribute:target_class/Value:RoutineChange' => 'RoutineChange',
+    'Class:MailInboxStandard/Attribute:target_class/Value:RoutineChange+' => '',
+    'Class:MailInboxStandard/Attribute:target_class/Value:NormalChange' => 'NormalChange',
+    'Class:MailInboxStandard/Attribute:target_class/Value:NormalChange+' => '',
+    'Class:MailInboxStandard/Attribute:target_class/Value:EmergencyChange' => 'EmergencyChange',
+    'Class:MailInboxStandard/Attribute:target_class/Value:EmergencyChange+' => '',
+    'Class:MailInboxStandard/Attribute:target_class/Value:Problem' => 'Problem',
+    'Class:MailInboxStandard/Attribute:target_class/Value:Problem+' => '',
+    'Class:MailInboxStandard/Attribute:target_class+' => 'Which class of Ticket to create or update when a new eMail arrives in this inbox. Only one class is possible.',
 
 	'Class:MailInboxStandard/Attribute:ticket_default_values' => 'Ticket Default Values',
+    'Class:MailInboxStandard/Attribute:ticket_default_values+' => "Provide a value for all the mandatory fields at ticket creation.
+Title, caller and description are already managed.
+One field per line using the format: <field_code>:<value>
+When setting external keys such as 'org_id' use the id (or the friendly name - less robust).",
+    'Class:MailInboxStandard/Attribute:ticket_default_values?' => 'One initialization per line, in format <field_code>:<value>',
+
+
 	'Class:MailInboxStandard/Attribute:ticket_default_title' => 'Default Title (if subject is empty)',
+    'Class:MailInboxStandard/Attribute:ticket_default_title+' => 'The subject of the incoming eMail is used to feed the title on ticket creation. 
+This one is a fallback, used only if the eMail subject is empty',
+
 	'Class:MailInboxStandard/Attribute:title_pattern+' => 'Pattern to match in the subject',
 	'Class:MailInboxStandard/Attribute:title_pattern' => 'Title Pattern',
 	'Class:MailInboxStandard/Attribute:title_pattern?' => 'Use PCRE syntax, including starting and ending delimiters',
 
 	'Class:MailInboxStandard/Attribute:stimuli' => 'Stimuli to apply',
-	'Class:MailInboxStandard/Attribute:stimuli+' => 'Apply a stimulus when the ticket is in a given state',
-	'Class:MailInboxStandard/Attribute:stimuli?' => 'Use the format <state_code>:<stimulus_code>',
+	'Class:MailInboxStandard/Attribute:stimuli+' => 'Define the stimulus to apply for each ticket state.
+One state/stimulus per line, using format <state_code>:<stimulus_code>',
+	'Class:MailInboxStandard/Attribute:stimuli?' => 'One state/stimulus per line, using format <state_code>:<stimulus_code>',
 
-	'Class:MailInboxStandard/Attribute:unknown_caller_behavior' => 'Behavior in case of Unknown Caller',
+	'Class:MailInboxStandard/Attribute:unknown_caller_behavior' => 'In case of unknown sender',
 	'Class:MailInboxStandard/Attribute:unknown_caller_behavior/Value:create_contact' => 'Create a new Person',
 	'Class:MailInboxStandard/Attribute:unknown_caller_behavior/Value:reject_email' => 'Reject the eMail',
+    'Class:MailInboxStandard/Attribute:unknown_caller_behavior+' => 'Behavior when the sender email is not found in the '.ITOP_APPLICATION_SHORT.' Persons:
+ - Create a new Person: with the sender email and the "New Person\'s Default Values"
+ - Reject the eMail: flag the eMail in error and reply to the sender with the content of "Unknown senders rejection reply"',
 
-    'Class:MailInboxStandard/Attribute:unknown_caller_rejection_reply' => 'Unknown Caller rejection reply',
+    'Class:MailInboxStandard/Attribute:unknown_caller_rejection_reply' => 'Unknown senders rejection reply',
+    'Class:MailInboxStandard/Attribute:unknown_caller_rejection_reply+' => 'Optional reply to sender if we decided to “Reject the eMail” in case of unknown sender.
+Unknown senders are email addresses which does not correspond to any Person in '.ITOP_APPLICATION_SHORT.'.
+If this field is left empty, then no message is sent to unknown senders',
 
 	'Class:MailInboxStandard/Attribute:trace' => 'Debug trace',
 	'Class:MailInboxStandard/Attribute:trace/Value:yes' => 'Yes',
 	'Class:MailInboxStandard/Attribute:trace/Value:no' => 'No',
+    'Class:MailInboxStandard/Attribute:trace+' => '	Use this to track the various operations performed while processing eMails from this inbox.
+Do not activate this option for long periods on production since it tends to generate a lot of output which slows down the server',
 	
 	'Class:MailInboxStandard/Attribute:import_additional_contacts' => 'Add more contacts (To, CC)',
 	'Class:MailInboxStandard/Attribute:import_additional_contacts/Value:never' => 'Never',
 	'Class:MailInboxStandard/Attribute:import_additional_contacts/Value:only_on_creation' => 'When creating a new Ticket',
 	'Class:MailInboxStandard/Attribute:import_additional_contacts/Value:only_on_update' => 'When updating an existing Ticket',
 	'Class:MailInboxStandard/Attribute:import_additional_contacts/Value:always' => 'Always',
+    'Class:MailInboxStandard/Attribute:import_additional_contacts+' => 'Search for '.ITOP_APPLICATION_SHORT.' contacts having the email address present in To and CC of the received eMail message, and link them to the Ticket.
+Already linked contacts are ignored. Unknown email addresses are ignored.',
 		
 	'Class:MailInboxStandard/Attribute:caller_default_values' => "New Person's Default Values",
-	'Class:MailInboxStandard/Attribute:debug_log' => 'Debug Log',
+	'Class:MailInboxStandard/Attribute:caller_default_values+' => 'Provide a value for all mandatory fields of a Person, except email.
+Use one field initialization per line, in the format: <field_code>:<value>',
+    'Class:MailInboxStandard/Attribute:caller_default_values?' => 'One setting per line, in format <field_code>:<value>',
+
+    'Class:MailInboxStandard/Attribute:debug_trace' => 'Debug trace',
+    'Class:MailInboxStandard/Attribute:debug_trace+' => '',
+
 	'Class:MailInboxStandard/Attribute:error_behavior' => 'Behavior in case of error',
-	'Class:MailInboxStandard/Attribute:error_behavior+' => 'For the messages too big (size > \'maximum_email_size\') the behavior is to mark them as error and to keep them in the mailbox',
 	'Class:MailInboxStandard/Attribute:error_behavior/Value:delete' => 'Delete the message from the mailbox',
 	'Class:MailInboxStandard/Attribute:error_behavior/Value:mark_as_error' => 'Keep the message in the mailbox',
+	'Class:MailInboxStandard/Attribute:error_behavior+' => 'Root causes of eMails in error: 
+- Messages too big (size > \'maximum_email_size\'),
+- Unknown sender (email address not found in '.ITOP_APPLICATION_SHORT.' Persons),
+- eMail format not supported (e.g. encrypted, unknown, etc.),',
+
 	'Class:MailInboxStandard/Attribute:notify_errors_to' => 'Forward eMails To',
+	'Class:MailInboxStandard/Attribute:notify_errors_to+' => 'The email address to which to forward emails in error, as an attachment. 
+If this address is left empty and if the emails in error are deleted, then nobody will notice the issue and be able to troubleshoot.',
 	'Class:MailInboxStandard/Attribute:notify_errors_from' => '(From)',
+	'Class:MailInboxStandard/Attribute:notify_errors_from+' => 'The email address to be used as the “sender” of error notifications. 
+For security reasons, many mail servers only relay messages with a define sender address.',
+
+// Fieldsets and Tabs
+
 	'MailInbox:Server' => 'Mailbox Configuration',
 	'MailInbox:Behavior' => 'Behavior on Incoming eMails',
-	'MailInbox:Caller' => 'Unknown Callers',
+	'MailInbox:Caller' => 'Unknown Senders',
 	'MailInbox:Errors' => 'Emails in Error',
 	'MailInbox:OtherContacts' => 'Behavior for Additional Contacts',
 	'Menu:MailInboxes' => 'Incoming eMail Inboxes',
@@ -86,23 +146,4 @@ Dict::Add('EN US', 'English', 'English', array(
 	'MailInboxStandard:DebugTrace' => 'Debug Trace',
 	'MailInboxStandard:DebugTraceNotActive' => 'Activate the debug on this Inbox to see the debug trace here.',
 	'MailInbox:NoSubject' => 'No subject',
-));
-
-//
-// Class: MailInboxStandard
-//
-
-Dict::Add('EN US', 'English', 'English', array(
-	'Class:MailInboxStandard/Attribute:target_class/Value:Change' => 'Change',
-	'Class:MailInboxStandard/Attribute:target_class/Value:Change+' => '',
-	'Class:MailInboxStandard/Attribute:target_class/Value:RoutineChange' => 'RoutineChange',
-	'Class:MailInboxStandard/Attribute:target_class/Value:RoutineChange+' => '',
-	'Class:MailInboxStandard/Attribute:target_class/Value:NormalChange' => 'NormalChange',
-	'Class:MailInboxStandard/Attribute:target_class/Value:NormalChange+' => '',
-	'Class:MailInboxStandard/Attribute:target_class/Value:EmergencyChange' => 'EmergencyChange',
-	'Class:MailInboxStandard/Attribute:target_class/Value:EmergencyChange+' => '',
-	'Class:MailInboxStandard/Attribute:target_class/Value:Problem' => 'Problem',
-	'Class:MailInboxStandard/Attribute:target_class/Value:Problem+' => '',
-	'Class:MailInboxStandard/Attribute:debug_trace' => 'Debug trace',
-	'Class:MailInboxStandard/Attribute:debug_trace+' => '',
 ));
--- fr.dict.itop-standard-email-synchro.php
+++ fr.dict.itop-standard-email-synchro.php
@@ -1,31 +1,50 @@
 <?php
-// Copyright (C) 2010-2024 Combodo SAS
+/**
+ * Localized data
+ *
+ * @copyright Copyright (C) 2010-2024 Combodo SAS
+ * @license	http://opensource.org/licenses/AGPL-3.0
+ *
+ * This file is part of iTop.
+ *
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with iTop. If not, see <http://www.gnu.org/licenses/>
+ */
+
 //
-//   This program is free software; you can redistribute it and/or modify
-//   it under the terms of the GNU Lesser General Public License as published by
-//   the Free Software Foundation; version 3 of the License.
+// Class: MailInboxStandard
 //
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with this program; if not, write to the Free Software
-//   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 Dict::Add('FR FR', 'French', 'Français', array(
 	// Dictionary entries go here
 	'Class:MailInboxStandard' => 'Boîte Mail Standard',
 	'Class:MailInboxStandard+' => 'Source d\'eMails',
-	'Class:MailInboxStandard/Attribute:behavior' => 'Comportement',
-	'Class:MailInboxStandard/Attribute:behavior/Value:create_only' => 'Créer des Tickets',
-	'Class:MailInboxStandard/Attribute:behavior/Value:update_only' => 'Mettre à jour des Tickets existants',
-	'Class:MailInboxStandard/Attribute:behavior/Value:both' => 'Créer ou mettre à jour des Tickets',
+
+	'Class:MailInboxStandard/Attribute:behavior' => 'A réception d\'un eMail',
+	'Class:MailInboxStandard/Attribute:behavior/Value:create_only' => 'Créer un Ticket',
+	'Class:MailInboxStandard/Attribute:behavior/Value:update_only' => 'Mettre à jour un Ticket existant',
+	'Class:MailInboxStandard/Attribute:behavior/Value:both' => 'Créer ou mettre à jour un Ticket',
+    'Class:MailInboxStandard/Attribute:behavior+' => 'Comportement lorsqu\'un nouveau message arrives dans la boîte mail:
+- Créer ou mettre à jour un Ticket : met à jour le Ticket correspondant, sinon en crée un nouveau.
+- Créer un Ticket : Crée systèmatiquement un nouveau ticket pour chaque message reçu.
+- Mettre à jour un Ticket existant : met à jour le Ticket correspondant, sinon marque le message en erreur.',
 
 	'Class:MailInboxStandard/Attribute:email_storage' => 'Après traitement de l\'eMail',
 	'Class:MailInboxStandard/Attribute:email_storage/Value:keep' => 'Conserver l\'eMail sur le serveur',
 	'Class:MailInboxStandard/Attribute:email_storage/Value:delete' => 'Effacer immédiatement l\'eMail',
 	'Class:MailInboxStandard/Attribute:email_storage/Value:move' => 'Déplacer vers un autre dossier',
+    'Class:MailInboxStandard/Attribute:email_storage+' => 'Choisir l\'action à effectuer après le traitement réussi d\'un eMail entrant.
+Les eMails en erreur ne sont pas concernés par ce choix. Leur traitement dépend du paramètre \'Comportement en cas d\'erreur\'.',
 
 	'Class:MailInboxStandard/Attribute:target_folder' => 'Dossier cible',
 	'Class:MailInboxStandard/Attribute:target_folder+' => 'Utilisé uniquement pour déplacer un e-mail avec le protocole IMAP',
@@ -33,41 +52,90 @@ Dict::Add('FR FR', 'French', 'Français', array(
 	'Class:MailInboxStandard/Attribute:target_class' => 'Type de Ticket',
 	'Class:MailInboxStandard/Attribute:target_class/Value:Incident' => 'Incident',
 	'Class:MailInboxStandard/Attribute:target_class/Value:UserRequest' => 'Demande utilisateur',
+	'Class:MailInboxStandard/Attribute:target_class/Value:Change' => 'Ticket de Changement',
+	'Class:MailInboxStandard/Attribute:target_class/Value:Change+' => '',
+	'Class:MailInboxStandard/Attribute:target_class/Value:RoutineChange' => 'Changement de Routine',
+	'Class:MailInboxStandard/Attribute:target_class/Value:RoutineChange+' => '',
+	'Class:MailInboxStandard/Attribute:target_class/Value:NormalChange' => 'Changement Normal',
+	'Class:MailInboxStandard/Attribute:target_class/Value:NormalChange+' => '',
+	'Class:MailInboxStandard/Attribute:target_class/Value:EmergencyChange' => 'Changement en urgence',
+	'Class:MailInboxStandard/Attribute:target_class/Value:EmergencyChange+' => '',
+	'Class:MailInboxStandard/Attribute:target_class/Value:Problem' => 'Problème',
+	'Class:MailInboxStandard/Attribute:target_class/Value:Problem+' => '',
+    'Class:MailInboxStandard/Attribute:target_class+' => 'Quel type de Ticket sera créé ou mis à jour par les messages reçus sur cette boîte mail ? Un seul type possible.',
 
 	'Class:MailInboxStandard/Attribute:ticket_default_values' => 'Valeurs par défaut du Ticket',
+    'Class:MailInboxStandard/Attribute:ticket_default_values+' => 'Initialiser les champs obligatoires en création du ticket avec des valeurs par défaut.
+Le titre, le demandeur, l\'organisation, la description et l\'origine sont déjà gérés.
+Une ligne par champ, au format format: <code_attribut>:<valeur>
+Pour initialiser des clés externes comme \'org_id\', mettre comme valeur l\'id (ou le nom complet - moins pérenne).
+Limitation connue : service_id:Service Médical ⇒ n\'est pas reconnu à cause des accents.',
+    'Class:MailInboxStandard/Attribute:ticket_default_values?' => 'Un champ par ligne, au format <code_attribut>:<valeur>',
+
 	'Class:MailInboxStandard/Attribute:ticket_default_title' => 'Titre par défaut (en cas de sujet vide)',
+    'Class:MailInboxStandard/Attribute:ticket_default_title+' => 'Le sujet de l\'eMail est copié dans le titre du ticket lors de la création. 
+ Si le sujet de l\'eMail reçu est vide, alors ce titre par défaut sera utilisé pour créer le ticket',
+
 	'Class:MailInboxStandard/Attribute:title_pattern+' => 'Expression régulière à rechercher dans l\'objet de l\'eMail',
 	'Class:MailInboxStandard/Attribute:title_pattern' => 'Recherche dans l\'objet du mail (RegExp)',
 	'Class:MailInboxStandard/Attribute:title_pattern?' => 'Utilisez la syntaxe PCRE avec les délimiteurs de début et de fin',
 
 	'Class:MailInboxStandard/Attribute:stimuli' => 'Stimuli à appliquer',
-	'Class:MailInboxStandard/Attribute:stimuli+' => 'Appliquer un stimulus quand le ticket est dans un état donné',
-	'Class:MailInboxStandard/Attribute:stimuli?' => 'Utilisez le format <code_etat>:<code_stimulus>',
+	'Class:MailInboxStandard/Attribute:stimuli+' => 'Définir pour chaque état du ticket le stimulus à appliquer à la réception d\'un message.
+Un couple état/stimulus par ligne, au format <code_etat>:<code_stimulus>',
+	'Class:MailInboxStandard/Attribute:stimuli?' => 'Un couple état/stimulus par ligne, au format <code_etat>:<code_stimulus>',
 
-	'Class:MailInboxStandard/Attribute:unknown_caller_behavior' => 'Comportement pour les contacts inconnus',
+	'Class:MailInboxStandard/Attribute:unknown_caller_behavior' => 'En cas d\'expéditeur inconnu',
 	'Class:MailInboxStandard/Attribute:unknown_caller_behavior/Value:create_contact' => 'Créer une nouvelle Personne',
 	'Class:MailInboxStandard/Attribute:unknown_caller_behavior/Value:reject_email' => 'Rejeter l\'eMail',
+    'Class:MailInboxStandard/Attribute:unknown_caller_behavior+' => 'Si l\'expéditeur de l\'eMail n\'est pas reconnu, que faire :
+ - Créer une nouvelle Personne : dans '.ITOP_APPLICATION_SHORT.' avec l\'eMail de l\'expéditeur et les \'Valeurs par défaut pour la nouvelle Personne\'
+ - Rejeter l\'eMail : ce qui le marquera en erreur et répondra avec le contenu du champ \'Réponse aux expéditeurs inconnus\'.',
 
-    'Class:MailInboxStandard/Attribute:unknown_caller_rejection_reply' => 'Réponse pour les contacts inconnus',
+    'Class:MailInboxStandard/Attribute:unknown_caller_rejection_reply' => 'Réponse aux expéditeurs inconnus',
+    'Class:MailInboxStandard/Attribute:unknown_caller_rejection_reply+' => 'Ce champ optionnel spécifie le message à envoyer aux expéditeurs inconnus.
+Un expéditeur inconnu est celui dont l\'adresse mail ne correspond à aucune Personne dans '.ITOP_APPLICATION_SHORT.'.
+Si ce champ est laissé vide, alors aucune réponse ne leur est envoyée.',
 
 	'Class:MailInboxStandard/Attribute:trace' => 'Activer la trace',
 	'Class:MailInboxStandard/Attribute:trace/Value:yes' => 'Oui',
 	'Class:MailInboxStandard/Attribute:trace/Value:no' => 'Non',
-	
+    'Class:MailInboxStandard/Attribute:trace+' => '	Permet d\'enregistrer les diffèrentes opérations effectuées lors du traitement des eMails reçus par cette boîte mail.
+Ne laissez pas cette option activée trop longtemps dans un environnement de production, car elle génére une grande quantité de données qui peut ralentir votre serveur',
+
 	'Class:MailInboxStandard/Attribute:import_additional_contacts' => 'Ajouter des contacts (To, CC)',
 	'Class:MailInboxStandard/Attribute:import_additional_contacts/Value:never' => 'Jamais',
 	'Class:MailInboxStandard/Attribute:import_additional_contacts/Value:only_on_creation' => 'Lors de la création d\'un nouveau Ticket',
 	'Class:MailInboxStandard/Attribute:import_additional_contacts/Value:only_on_update' => 'Lors de la mise à jour d\'un Ticket existant',
 	'Class:MailInboxStandard/Attribute:import_additional_contacts/Value:always' => 'Toujours',
+    'Class:MailInboxStandard/Attribute:import_additional_contacts+' => 'Rechercher les contacts ayant comme email, l\'adresse d\'un destinataire du message, et les lier au Ticket.
+Les contacts déjà liés sont ignorés. Les adresses mail inconnues sont ignorées.',
 		
 	'Class:MailInboxStandard/Attribute:caller_default_values' => 'Valeurs par défaut pour la nouvelle Personne',
-	'Class:MailInboxStandard/Attribute:debug_log' => 'Traces de Debug',
+	'Class:MailInboxStandard/Attribute:caller_default_values+' => 'Fournir une valeur pour tous les champs obligatoires de la Personne, sauf l\'email.
+Un champ par ligne, au format <code_attribut>:<valeur>',
+    'Class:MailInboxStandard/Attribute:caller_default_values?' => 'Un champ par ligne, au format <code_attribut>:<valeur>',
+
+    'Class:MailInboxStandard/Attribute:debug_trace' => 'Debug trace',
+    'Class:MailInboxStandard/Attribute:debug_trace+' => '',
+
 	'Class:MailInboxStandard/Attribute:error_behavior' => 'Comportement en cas d\'erreur',
-	'Class:MailInboxStandard/Attribute:error_behavior+' => 'Pour les eMails trop gros (taille > \'maximum_email_size\') le comportement est toujours de les marquer en erreur et de les garder dans la boîte mail',
 	'Class:MailInboxStandard/Attribute:error_behavior/Value:delete' => 'Supprimer l\'eMail de la boîte mail',
 	'Class:MailInboxStandard/Attribute:error_behavior/Value:mark_as_error' => 'Garder l\'eMail dans la boîte mail',
+    'Class:MailInboxStandard/Attribute:error_behavior+' => 'Causes les plus fréquentes de mail en erreur :
+- Les messages dont la taille dépasse le paramètre \'maximum_email_size\'.
+- Les messages dont l\'expéditeur ne correspond pas à une Personne dans '.ITOP_APPLICATION_SHORT.'
+- Les messages dont le format n\'est pas supporté (chiffré, inconnu, etc.)',
+
 	'Class:MailInboxStandard/Attribute:notify_errors_to' => 'Faire suivre l\'eMail à',
+	'Class:MailInboxStandard/Attribute:notify_errors_to+' => 'L\'adresse mail à laquelle faire suivre les eMails en erreur. 
+Si cette adresse est vide et que les eMails en erreur sont supprimés, alors personne ne sera informé et aucune investigation ne pourra être mené.',
 	'Class:MailInboxStandard/Attribute:notify_errors_from' => '(De)',
+	'Class:MailInboxStandard/Attribute:notify_errors_from+' => 'L\'adresse mail à utiliser comme expéditeur des messages faisant suivre les eMails en erreur. 
+Si elle n\'est pas spécifiée, la plupart des serveurs mail détruiront ces messages pour des raisons de sécurité.',
+
+// Fieldsets and Tabs
+
 	'MailInbox:Server' => 'Configuration de la boîte mail',
 	'MailInbox:Behavior' => 'Comportement',
 	'MailInbox:Caller' => 'Contacts inconnus',
@@ -80,21 +148,3 @@ Dict::Add('FR FR', 'French', 'Français', array(
 	'MailInbox:NoSubject' => 'Pas de sujet',
 ));
 
-//
-// Class: MailInboxStandard
-//
-
-Dict::Add('FR FR', 'French', 'Français', array(
-	'Class:MailInboxStandard/Attribute:target_class/Value:Change' => 'Ticket de Changement',
-	'Class:MailInboxStandard/Attribute:target_class/Value:Change+' => '',
-	'Class:MailInboxStandard/Attribute:target_class/Value:RoutineChange' => 'Changement de Routine',
-	'Class:MailInboxStandard/Attribute:target_class/Value:RoutineChange+' => '',
-	'Class:MailInboxStandard/Attribute:target_class/Value:NormalChange' => 'Changement Normal',
-	'Class:MailInboxStandard/Attribute:target_class/Value:NormalChange+' => '',
-	'Class:MailInboxStandard/Attribute:target_class/Value:EmergencyChange' => 'Changement en urgence',
-	'Class:MailInboxStandard/Attribute:target_class/Value:EmergencyChange+' => '',
-	'Class:MailInboxStandard/Attribute:target_class/Value:Problem' => 'Problème',
-	'Class:MailInboxStandard/Attribute:target_class/Value:Problem+' => '',
-	'Class:MailInboxStandard/Attribute:debug_trace' => 'Debug trace',
-	'Class:MailInboxStandard/Attribute:debug_trace+' => '',
-));
